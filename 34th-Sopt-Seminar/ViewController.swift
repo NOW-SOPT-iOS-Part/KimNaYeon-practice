4fe94c6d3e9f40457e30a3c0840aba33725bfc82
Nya128 <nanalovesbap@gmail.com>
2024-03-30 14:17:04 +0900

    Initial Commit

---
+++ 34th-Sopt-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 김나연 on 3/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
